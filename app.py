97bb87bbd0a5d37be3b6c151e59292f8017bfada
Andrea Rogges <andrea.rogges@gmail.com>
2025-04-07 14:16:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def estrai_dati_da_pdf(lista_file_pdf):
     return pd.DataFrame(dati_completi, columns=colonne_selezionate)
 
 st.title("Estrazione Tabelle da PDF FasiOpen")
-st.info("Build 1.3 - 02/04/2025")
+st.info("Build 1.3.1 - 07/04/2025")
 
 file_caricati = st.file_uploader("Carica i file PDF o ZIP", type=["pdf","zip"], accept_multiple_files=True)
 
