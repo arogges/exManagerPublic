c3df50466eaa5c978269b4b55bcc915759cd824a
Andrea Rogges <andrea.rogges@gmail.com>
2025-06-06 15:38:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -261,7 +261,7 @@ def estrai_dati_nuovo_formato(lista_file_pdf, lista_nomi_pdf=None):
     return df, file_con_errori
 
 st.title("Estrazione Tabelle da PDF FasiOpen")
-st.info("Build 1.5.0 - 06/06/2025 - Supporto doppio formato")
+st.info("Build 1.5.1 - 06/06/2025 - Supporto doppio formato")
 
 # Creo due sezioni separate per i due tipi di file
 col1, col2 = st.columns(2)
